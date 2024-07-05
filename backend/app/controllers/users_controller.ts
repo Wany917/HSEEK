91f529bf36bbcde1f3c18ecbd70a6bce2f1b266a
Wany917 <yvannkakou03@gmail.com>
2024-07-05 15:43:44 +0200

    user log dot

--- backend/app/controllers/users_controller.ts
+++ backend/app/controllers/users_controller.ts
@@ -6,61 +6,61 @@ import { queryAllUsersValidator } from '#validators/user'
 import { updateProfileValidator } from '#validators/user'
 
 export default class UsersController {
-    async register({ request, response }: HttpContext) {
-        try {
-          const payload = await request.validateUsing(registerValidator)
-          const user = new User()
+  async register({ request, response }: HttpContext) {
+    try {
+      const payload = await request.validateUsing(registerValidator)
+      const user = new User()
 
-          user.merge(payload)
-          await user.save()
-    
-          return response.created(user)
-        } catch (error) {
-          console.log(error);
-          return response.status(400).send({ error: 'Registration failed', details: error.messages })
-        }
-      }
-    
-      async login({ request, response }: HttpContext) {
-        try {
-          const { email, password } = await request.validateUsing(loginValidator)
-          const user = await User.verifyCredentials(email, password)
-          const token = await User.accessTokens.create(user)
-    
-          return response.ok({
-            token: token.value!.release(),
-          })
-        } catch (error) {
-          return response.status(401).send({ error: 'Login failed', details: error.messages })
-        }
-      }
-    
-      async me({ auth, response }: HttpContext) {
-        const user = auth.user!
-        return response.ok(user)
-      }
-    
-      async index({ request, response }: HttpContext) {
-        const { page, perPage, username } = await request.validateUsing(queryAllUsersValidator)
-    
-        const query = User.query().where('username', 'like', `%${username}%`)
-        const users = await query.paginate(page, perPage)
-    
-        return response.ok(users)
-      }
-    
-      async show({ params, response }: HttpContext) {
-        const user = await User.findOrFail(params.id)
-        return response.ok(user)
-      }
-    
-      async update({ request, response, auth }: HttpContext) {
-        const user = auth.user!
-        const payload = await request.validateUsing(updateProfileValidator)
-    
-        user.merge(payload)
-        await user.save()
-    
-        return response.ok(user)
-      }
+      user.merge(payload)
+      await user.save()
+
+      return response.created(user)
+    } catch (error) {
+      console.log(error)
+      return response.status(400).send({ error: 'Registration failed', details: error.messages })
+    }
+  }
+
+  async login({ request, response }: HttpContext) {
+    try {
+      const { email, password } = await request.validateUsing(loginValidator)
+      const user = await User.verifyCredentials(email, password)
+      const token = await User.accessTokens.create(user)
+
+      return response.ok({
+        token: token.value!.release(),
+      })
+    } catch (error) {
+      return response.status(401).send({ error: 'Login failed', details: error.messages })
+    }
+  }
+
+  async me({ auth, response }: HttpContext) {
+    const user = auth.user!
+    return response.ok(user)
+  }
+
+  async index({ request, response }: HttpContext) {
+    const { page, perPage, username } = await request.validateUsing(queryAllUsersValidator)
+
+    const query = User.query().where('username', 'like', `%${username}%`)
+    const users = await query.paginate(page, perPage)
+
+    return response.ok(users)
+  }
+
+  async show({ params, response }: HttpContext) {
+    const user = await User.findOrFail(params.id)
+    return response.ok(user)
+  }
+
+  async update({ request, response, auth }: HttpContext) {
+    const user = auth.user!
+    const payload = await request.validateUsing(updateProfileValidator)
+
+    user.merge(payload)
+    await user.save()
+
+    return response.ok(user)
+  }
 }
